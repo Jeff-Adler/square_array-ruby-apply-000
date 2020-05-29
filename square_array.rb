27698ec2f4d99e23e18c140b51387a2e08112ce3
Jeff Adler <jeffm.adler@gmail.com>
2020-05-29 21:24:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
-  square_array = nil
+  square_array = []
+  counter = 0
   array.each do |number|
-    square_array << number**2
+    square_array[counter] << number**2
+    counter += 1
   end
   return square_array
 end
